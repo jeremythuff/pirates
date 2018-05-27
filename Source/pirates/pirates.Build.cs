861a64ebdc6e62186cdbebee2561682559b85dec
Jeremy Huff <jeremythuff@gmail.com>
2018-05-26 23:30:26 -0500

    Got steam working in package

--- Source/pirates/pirates.Build.cs
+++ Source/pirates/pirates.Build.cs
@@ -8,7 +8,7 @@ public class pirates : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "UnrealFastNoisePlugin", "OnlineSubsystem", "OnlineSubsystemUtils" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
